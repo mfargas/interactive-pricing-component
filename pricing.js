15c896c52a1de7cc32e7020f088d7ba56a2da9be
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-07-09 16:32:13 -0700

    Adjust layout of slider and js components

--- pricing.js
+++ pricing.js
@@ -1,29 +1,14 @@
 const priceBar = document.querySelectorAll(".price");
 
-function handlePricingSlide(input, output) {
-    // output the current slider value
-    if (input.currentValEl) input.currentValEl.innerHTML = input.data[input.el.value];
-    // output the current slider value
-    if (input.currentValEl)
-        input.currentValEl.innerHTML = input.data[input.el.value];
-    // update prices
-    for (let i = 0; i < output.length; i++) {
-        const outputObj = output[i];
-        if (outputObj.currency) outputObj.currency.innerHTML = outputObj.data[input.el.value][0];
-        if (outputObj.amount) outputObj.amount.innerHTML = outputObj.data[input.el.value][1];
-        if (outputObj.after) outputObj.after.innerHTML = outputObj.data[input.el.value][2];
-    }
-}
-
 if(priceBar.length > 0){
     for(let i = 0 ; i < priceBar.length ; i++){
         const slider = priceBar[i];
 
         //input
-        let pricingInput = {el: priceBar.querySelector("input")};
+        const pricingInput = {el: priceBar.querySelector("input")};
 
         pricingInput.data = JSON.parse(pricingInput.el.getAttribute("data-price-input"));
-        pricingInput.currentValEl = slider.querySelector(".pricing-slider-value");
+        pricingInput.currentValEl = slider.querySelector(".pageviews");
 
         pricingInputEl.setAttribute("min", 0);
         pricingInputEl.setAttribute("max", Object.keys(priceInput).length - 1);
@@ -32,17 +17,17 @@ if(priceBar.length > 0){
         pricingInputEl.setAttribute("value", 0);
 
         //output
-        let pricingOutputEl = slider.parentNode.querySelectorAll(".");
+        const pricingOutputEl = slider.parentNode.querySelectorAll(".price-comp");
 
-        let pricingOutput = [];
+        const pricingOutput = [];
         for(let i = 0 ; i < pricingOutputEl.length ; i++){
-            const el = pricingOutputEl[i];
-            const pricingOutputObj = {};
+            const element = pricingOutputEl[i];
+            const outputObj = {};
             
-            pricingOutputObj.currency = el.querySelector(".price-currency");
-            pricingOutputObj.amount = el.querySelector(".price-amount");
-            pricingOutputObj.after = el.querySelector(".price-option");
-            pricingOutputObj.data = JSON.parse(el.getAttribute("data-price-output"));
+            pricingOutputObj.currency = element.querySelector(".price-currency");
+            pricingOutputObj.amount = element.querySelector(".price-amount");
+            pricingOutputObj.after = element.querySelector(".price-option");
+            pricingOutputObj.data = JSON.parse(element.getAttribute("data-price-output"));
             pricingOutput.push(pricingOutputObj);
         }
 
@@ -56,4 +41,22 @@ if(priceBar.length > 0){
 function updatePageviews(val){
     let output = document.querySelector(".pageviews");
     output.innerText = val;
+}
+
+function handlePricingSlide(input, output) {
+
+    // output the current slider value
+    if (input.currentValEl) input.currentValEl.innerHTML = input.data[input.el.value];
+    
+    // output the current slider value
+    if (input.currentValEl)
+        input.currentValEl.innerHTML = input.data[input.el.value];
+    
+    // update prices
+    for (let i = 0; i < output.length; i++) {
+        const outputObj = output[i];
+        if (outputObj.currency) outputObj.currency.innerHTML = outputObj.data[input.el.value][0];
+        if (outputObj.amount) outputObj.amount.innerHTML = outputObj.data[input.el.value][1];
+        if (outputObj.after) outputObj.after.innerHTML = outputObj.data[input.el.value][2];
+    }
 }
